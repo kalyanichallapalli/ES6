3282034f6e8e7b082facf3983d20db6e80b4787d
Kalyani <kalyanireddy9@gmail.com>
2021-11-17 20:48:23 -0800

    new changes

---
+++ Es6/es6.arrow.js
@@ -0,0 +1,9 @@
+//Arrow Functions:Arrow functions allows a short syntax for writing function expressions.
+// ES5
+var x = function(x, y) {
+    return x * y;
+ }
+ console.log(x(2, 3))
+ // ES6
+ const y = (x, y) => x * y;
+ console.log(y(2, 5))
---
+++ Es6/es6.loop.js
@@ -0,0 +1,20 @@
+
+//The For/Of Loop
+//Looping over an Array
+const cars = ["BMW", "Volvo", "Mini"];
+let text = "";
+
+for (let x of cars) {
+  text += x + " ";
+}
+
+console.log(text)
+//Looping over a String
+let language = "JavaScript";
+let text1 = "";
+
+for (let x of language) {
+    text1 += x + " ";
+}
+
+console.log(text1)
