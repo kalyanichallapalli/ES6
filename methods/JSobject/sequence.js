a52657aa36b7dac95bb1534f15d5658ecda5f490
Kalyani <kalyanireddy9@gmail.com>
2021-12-05 15:06:42 -0800

    new

--- methods/JSobject/sequence.js
+++ methods/JSobject/sequence.js
@@ -9,3 +9,5 @@ function sorted(num){
 console.log(sorted([100,101,102,103,104]))
 console.log(sorted([120,40,20,300]))
 
+
+
--- methods/JSobject/sort.js
+++ methods/JSobject/sort.js
@@ -1,8 +1,7 @@
 //in an arry findout all are  sorted 
-function sorted(num){
+/* function sorted(num){
     let j;
 	for(let i = 0; i< num.length; i++){
-        console.log(num[i])
   	  j = i + 1;
       if(num[j] - num[i] < 0) return false;
     }
@@ -10,6 +9,5 @@ function sorted(num){
 }
 
 console.log(sorted([2,3,4,5,6,7]))
-console.log(sorted([1,2,3,4,6,5]))
-
+console.log(sorted([1,2,3,4,6,5])) */
 
