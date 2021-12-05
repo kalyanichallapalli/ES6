6d375176b8329ab5bd8fa90afbb12f3069a65769
Kalyani <kalyanireddy9@gmail.com>
2021-12-04 21:01:21 -0800

    change

---
+++ methods/JSobject/objectmethods/map.js
@@ -0,0 +1,69 @@
+// Create a Map using new MAP() method
+const fruits = new Map([
+    ["apples", 500],
+    ["bananas", 300],
+    ["oranges", 200]
+  ]);
+//Add the values to  MAP using Set() method
+
+const fruit = new Map();
+
+    fruit.set("apple",500);
+    fruit.set("mango",300);
+    fruit.set("grapes",400);
+    console.log(fruit)
+
+ //To get the vaule  of a key use Map.get()
+ 
+ console.log(fruit.get('apple'));
+
+ //Map.size: will return the no of values in Map
+ console.log(fruit.size);
+
+ //Map.delete mehtods delete elements in Map
+ fruit.delete("apple");
+ console.log(fruit)
+
+ //Map.clear() if you want to del all elemenyts in the Map
+ fruits.clear();
+ console.log(fruits)
+
+ //Map.has() method will find the element existence if its there or not 
+ console.log(fruit.has("mango"));
+
+ //typeof map returns object:
+ console.log(typeof fruits)
+ //instanceof Map ??//?////////////////////////////??????
+
+ //Map.forEach(): invokes a callback for each key/value pait in the map
+ const vegitables = new Map([
+     ["carrot", 100],["eggplant", 200],["beans", 500]
+    ]); 
+ let text = "";
+ vegitables.forEach (function(value, key) {
+    text += key +' = '+ value;
+    
+  })
+
+  //Map.keys()?????????????
+
+  //Map.values() ??????????
+
+  //Map.entries()
+
+  console.log(text)
+
+
+
+
+
+
+ 
+
+
+    
+
+
+
+
+
