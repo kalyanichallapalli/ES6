b5850d1b8de279090a718cf0fb4b744cfdfaacdd
Kalyani <kalyanireddy9@gmail.com>
2021-12-04 19:59:48 -0800

    obj

---
+++ methods/JSobject/objectmethods/object.js
@@ -0,0 +1,28 @@
+//create  object
+let name = {
+      first_name : 'kalyani',
+      last_name  : 'challapalli',
+      age        :  36
+}
+//accessing accessing an object
+let fname = name.first_name;
+let lname = name["last_name"];
+console.log(fname,lname)
+
+//object with function def 
+const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+  //accessing object function 
+  let name1=person.fullName();
+  console.log(person.firstName)
+  console.log(name1)
+ 
+
+  
+ 
