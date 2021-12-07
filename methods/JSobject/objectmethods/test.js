54492e843d3f01b819f542e4bbcee90aab5caf95
Kalyani <kalyanireddy9@gmail.com>
2021-12-06 23:06:44 -0800

    new

--- methods/JSobject/objectmethods/test.js
+++ methods/JSobject/objectmethods/test.js
@@ -4,15 +4,29 @@ function string1(char1) {
     var visitedarr1 = new Set();
     console.log(visitedarr1)
     for (let i=0;i<arr1.length;i++){
-        var first = arr1[i];
-        var next = arr1[i+1];
-        if (visitedarr1.has(next)) {
-           
-        } else {
-            visitedarr1.add(first); //A
-        }
+        visitedarr1.add(arr1[i]);
     }
+
     return visitedarr1;
 }
 console.log(string1("ABABBC"))
 console.log(string1("BABBCA"))
+
+
+function characterCount(myString) {
+    var arr1 = myString.split('');
+    var map = new Map();
+    for (let i=0;i<arr1.length;i++){
+        var count = 0;
+        if (map.has(arr1[i])) {
+            count = map.get(arr1[i]);
+            count = count +1;
+        } else {
+            count = 1;
+        }
+        map.set(arr1[i], count);
+    }
+    return map;
+}
+
+console.log(characterCount('ABCDABC'))
