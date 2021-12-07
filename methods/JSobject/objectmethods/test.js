c72652c2357c54020d07a98a734d553f58dd823b
Kalyani <kalyanireddy9@gmail.com>
2021-12-06 22:19:48 -0800

    new

--- methods/JSobject/objectmethods/test.js
+++ methods/JSobject/objectmethods/test.js
@@ -2,6 +2,7 @@ function string1(char1) {
     var arr1 = char1.split('');
     console.log(arr1)
     var visitedarr1 = new Set();
+    console.log(visitedarr1)
     for (let i=0;i<arr1.length;i++){
         var first = arr1[i];
         var next = arr1[i+1];
