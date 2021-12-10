96215908612df56a8bc4798954aa9d3a22e268cd
Kalyani <kalyanireddy9@gmail.com>
2021-12-09 20:51:46 -0800

    m

---
+++ methods/JSobject/objectmethods/tejotest/clousers.js
@@ -0,0 +1,9 @@
+//JavaScript Closures
+//in javascript variables can belongs to local or  grobel scope 
+//using CLOSURES  we can use grobel scope variables as local scope 
+
+
+
+
+
+  
--- methods/JSobject/objectmethods/tejotest/homework.js
+++ methods/JSobject/objectmethods/tejotest/homework.js
@@ -1,7 +1,14 @@
-let dolphinsscore = [44,23,71];
+//1) using arrow function 
+// let calcAverage = (array) => array.reduce((a,b) => a+b)/array.length
+
+//     console.log(calcAverage([44,23,71]))
+//     console.log(calcAverage([65,54,49]))
 
 
+function average(arr1, arr2) {
+    console.log(arr1, arr2) 
+}    
+console.log(average([44,23,71],[65,54,49]))
 
-console.log(average)
 
 
