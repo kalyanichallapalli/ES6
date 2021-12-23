28da2d3673990a67d3ef273d29789ca3f455fbad
Kalyani <kalyanireddy9@gmail.com>
2021-12-23 15:46:07 -0800

    in_test2 changes

---
+++ methods/JSobject/objectmethods/CODING-SESSION/in_test2.js
@@ -0,0 +1,18 @@
+function sayTorF(string1,string2){
+  let result = string1.toUpperCase();
+  let num = result.length - string2.length;
+  count = 0; 
+ for(i=0; i<result.length; i++) {
+    if(!string2.includes(result[i])){
+        count++;
+     };
+     if(count > num)
+     return false;
+ }
+return true
+}
+console.log(sayTorF('ebCde','BCD'))
+console.log(sayTorF('eCB','ECB'))
+console.log(sayTorF('BcdEF','BCEF'))
+console.log(sayTorF('BCD','BcD'))
+console.log(sayTorF('ebCde','EBCDE'))
--- methods/JSobject/objectmethods/tejotest/adjecentmul.js
+++ methods/JSobject/objectmethods/tejotest/adjecentmul.js
@@ -1,8 +1,8 @@
 function solution(inputArray){
     let a = 0;
     let b = 0;
-    let p = 0;
-    for(let i=0; i<inputArray.length; i++){
+    let p = -9999;
+    for(let i=0; i<inputArray.length-1; i++){
         a=inputArray[i];
         b=inputArray[i+1];  
        if((a*b) > p){
@@ -13,3 +13,6 @@ function solution(inputArray){
     return p;
 };
 console.log(solution([3, 6, -2, -5, 7, 3]))
+console.log(solution([-23, 4, -3, 8, -12]))
+
+
