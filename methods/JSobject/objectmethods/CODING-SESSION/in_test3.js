8a9c87881f4d26134f0e3c3bc694deb2856477ac
Kalyani <kalyanireddy9@gmail.com>
2022-01-03 11:44:58 -0800

    new

--- methods/JSobject/objectmethods/CODING-SESSION/in_test3.js
+++ methods/JSobject/objectmethods/CODING-SESSION/in_test3.js
@@ -12,7 +12,8 @@ function asciicode(arra1){
                                   
   var asciivalue = [];
   for(let i =0; i<arra1.length; i++){
-  asciivalue [i] = {[i]: String.fromCharCode(arra1[i])};  
+  let key = "" + arra1[i] + "";
+  asciivalue [i] = {[key]: String.fromCharCode(arra1[i])};  
  // asciicode.push({arra1[i]: String.fromCharCode(arra1[i])});
   }
  return asciivalue;
@@ -21,10 +22,10 @@ console.log(asciicode([118,117,120]))
 console.log(asciicode([101,121,110,113,113,103]));
 
 
-function name (arr) {
-let array = [];
-array.push(arr.map((val)=>{return {[val]:String.fromCharCode(val)}}));
-return array;
-}
-console.log(name([101,121,110,113,113,103]))
-console.log(name([118,117,120]))
+// function name (arr) {
+// let array = [];
+// array.push(arr.map((val)=>{return {[val]:String.fromCharCode(val)}}));
+// return array;
+// }
+// console.log(name([101,121,110,113,113,103]))
+// console.log(name([118,117,120]))
