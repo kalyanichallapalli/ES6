96f39437db1182ef688e80fa31cb4f476ba21ee0
Kalyani <kalyanireddy9@gmail.com>
2022-01-04 15:24:19 -0800

    new

--- methods/JSobject/objectmethods/CODING-SESSION/code3.js
+++ methods/JSobject/objectmethods/CODING-SESSION/code3.js
@@ -8,10 +8,23 @@ else{
     return false;
 }
 }
-console.log(isTriangle(1,2,2));
-console.log(isTriangle(7,2,2));
-console.log(isTriangle(9,2,2));
-console.log(isTriangle(7,2,3));
+// console.log(isTriangle(1,2,2));
+// console.log(isTriangle(7,2,2));
+// console.log(isTriangle(9,2,2));
+// console.log(isTriangle(7,2,3));
+function checkValidity(a, b, c) 
+{ 
+    // check condition 
+    if (a + b <= c || a + c <= b || b + c <= a) 
+        return false; 
+    else
+        return true; 
+} 
+// console.log(checkValidity(1,2,2))
+// console.log(checkValidity(7,2,2))
+// console.log(checkValidity(9,2,3))
+
+
 // You are going to be given a word. 
 // Your job is to return the middle character of the word.
 // If the word's length is odd, return the middle character. 
@@ -19,6 +32,28 @@ console.log(isTriangle(7,2,3));
 
 function getMiddle(s)
 {
-
+    var middleIndex = s.length / 2;
+    let a = s.length;
+     if((a%2 == 0)){
+        console.log(middleIndex)
+        return s.slice(middleIndex - 1, middleIndex + 1); //"TEST" : 1,3
+     }else{
+        console.log(middleIndex)
+        return s.slice(middleIndex,middleIndex + 1) // "TESTS": 2.5,3.5
+        
+     }
 }
+
+function getMiddle(s)
+{
+    let len = s.length;
+  return s.slice((len-1)/2, len/2+1);
+}
+
+console.log(getMiddle("TEST"))
+console.log(getMiddle("TESTS"))
+console.log(getMiddle("APPLE"))
+console.log(getMiddle("BANANA"))
+
+
 
---
+++ methods/JSobject/objectmethods/CODING-SESSION/code4.js
@@ -0,0 +1,16 @@
+// Given an input of an array of digits, return the array with each digit incremented by its position 
+// in the array: the first digit will be incremented by 1, the second digit by 2, etc. Make sure to start 
+// counting your positions from 1 ( and not 0 ).
+
+// Your result can only contain single digit numbers, so if adding a digit with its position gives you 
+// a multiple-digit number, only the last digit of the number should be returned.
+    
+function number(num){
+    let arr2 = [];
+for(let i=0; i<num.length; i++){
+  arr2.push((num[i]+i+1)%10);
+}
+return arr2;
+}
+console.log(number([1,2,3]));
+console.log(number([4, 6, 9, 1, 3]));
