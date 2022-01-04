772d6855c68e23da789190ec5efccf01a384690d
Kalyani <kalyanireddy9@gmail.com>
2022-01-04 11:38:33 -0800

    new

--- methods/JSobject/objectmethods/CODING-SESSION/code1.js
+++ methods/JSobject/objectmethods/CODING-SESSION/code1.js
@@ -15,3 +15,13 @@ console.log(smallEnough([1,2,3,4],5)) //true
 console.log(smallEnough([3,8,1], 8))
 console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100))  
 
+//USIMG MATH
+function smallEnough(a, limit){
+    return Math.max(...a) <= limit
+}
+console.log(smallEnough([1,2,3,4],3)) //false
+console.log(smallEnough([1,2,3,4],5)) //true
+// USING ARROW FUNCTIONS
+smallEnough = (a,limit) => a.every(e => e < limit);
+console.log(smallEnough([1,2,3,4],3)) //false
+console.log(smallEnough([1,2,3,4],5)) //true
