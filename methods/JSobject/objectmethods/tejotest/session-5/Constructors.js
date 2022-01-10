a3f814f8d5d32386c8a6caee698436bad8bcc715
Kalyani <kalyanireddy9@gmail.com>
2022-01-09 20:41:59 -0800

    new

--- methods/JSobject/objectmethods/tejotest/session-5/Constructors.js
+++ methods/JSobject/objectmethods/tejotest/session-5/Constructors.js
@@ -1,2 +1,2 @@
-//The constructor method is a special method of a class for creating and initializing an object instance of that class.
-//
+//The constructor method is a special method of a class for creating and initializing an object 
+//instance of that class.
