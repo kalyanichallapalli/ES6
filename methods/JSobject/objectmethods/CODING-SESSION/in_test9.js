df9446374a0c9fadc91e824ee066fc49f8ed2063
Kalyani <kalyanireddy9@gmail.com>
2022-01-10 19:55:23 -0800

    new

---
+++ methods/JSobject/objectmethods/CODING-SESSION/in_test9.js
@@ -0,0 +1,25 @@
+//Q: can you share question in the chat
+/*you have two array above (offers and sku) 
+
+task is to chekc for valid offers in first array ,then compare the id of valid offeres in second array
+ and  get true if the str property of the valid id is "selectedAll" , or else return false
+
+ */
+
+const { off } = require("process")
+
+let offers=[{id:4,valid:"yes",},
+            {id:5,valid:"yes",},
+            {id:6,valid:"No"},]
+
+
+let sku =[{id:4,str:"selected all"},
+          {id:5,str:"N/A"},
+          {id:6,str:"selected all"},]
+
+
+let find = offers.filter(a => sku.some(b => a.id === b.id && a.valid == "yes" && b.str == "selected all"));
+
+console.log(find)
+
+
--- methods/JSobject/objectmethods/tejotest/session-5/Constructors.js
+++ methods/JSobject/objectmethods/tejotest/session-5/Constructors.js
@@ -1,2 +1,11 @@
 //The constructor method is a special method of a class for creating and initializing an object 
 //instance of that class.
+class Polygon {
+    constructor() {
+      this.name = 'Polygon';
+    }
+  }
+  
+  const poly1 = new Polygon();
+  
+  console.log(poly1.name);
