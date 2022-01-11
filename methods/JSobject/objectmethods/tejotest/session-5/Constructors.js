3e091b02871a1f680397bbfc00d1898a90c72c53
Kalyani <kalyanireddy9@gmail.com>
2022-01-11 12:23:48 -0800

    new

--- methods/JSobject/objectmethods/tejotest/session-5/Constructors.js
+++ methods/JSobject/objectmethods/tejotest/session-5/Constructors.js
@@ -10,3 +10,13 @@
 let twoValues = new Rectangle(4,5);
 console.log(twoValues);
 console.log(twoValues.height)
+
+// class person {
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+//  let obj1 = new person("tejo", 40)
+//  console.log(obj1)
+//  console.log(obj1.age)
