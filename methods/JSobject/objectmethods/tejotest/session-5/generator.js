68e84a7deac4854e7653393c741a354c34969665
Kalyani <kalyanireddy9@gmail.com>
2022-01-11 13:14:47 -0800

    new

--- methods/JSobject/objectmethods/tejotest/session-5/generator.js
+++ methods/JSobject/objectmethods/tejotest/session-5/generator.js
@@ -1,1 +1,11 @@
- 
+const { name } = require("ejs");
+
+ class person{
+     constructorO(name, age){
+         this.name = name;
+         this.age = age;
+     }
+ }
+ //let objperson = new person("john",40);
+ let obj1 = new person("tejo", 40)
+console.log(obj1,name)
