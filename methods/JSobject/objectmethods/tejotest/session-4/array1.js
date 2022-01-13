23b3da0a875ef787da3ee64c65450388683d787b
Kalyani <kalyanireddy9@gmail.com>
2022-01-12 20:28:35 -0800

    new

--- methods/JSobject/objectmethods/tejotest/session-4/array1.js
+++ methods/JSobject/objectmethods/tejotest/session-4/array1.js
@@ -6,3 +6,30 @@ for(let i=0; i<arr1.length; i++){
 }
 
 console.log(arr2)
+
+//for of loop iirator 
+
+let s = [80, 90, 70];
+
+for (let i of s) {
+    i = i * 5;
+    console.log(i);
+}
+let colors = ['Red', 'Green', 'Blue'];
+for (const [index, color] of colors.entries()) {
+    console.log(`${color} is at index ${index}`);
+}
+
+//
+let scores = [1,2,3];
+scores.message = 'Hi';
+
+console.log("for...in:");
+for (let score in scores) {
+  console.log(score); 
+}
+
+console.log('for...of:');
+for (let score of scores) {
+  console.log(score);
+}
