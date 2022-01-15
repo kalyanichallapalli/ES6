0bb21a4c64f145f5b77712c3d2a67a482cdb0904
Kalyani <kalyanireddy9@gmail.com>
2022-01-15 13:03:18 -0800

    new

--- methods/JSobject/objectmethods/CODING-SESSION/50.js
+++ methods/JSobject/objectmethods/CODING-SESSION/50.js
@@ -151,3 +151,20 @@ function countDown(number) {
 }
 console.log(countDown(5))
 console.log(countDown(6))
+//Write a program that can perform arithmetic operations (calculator)?
+let numberr1 = 5
+let numberr2 = 4
+let operator = '+';
+let result = 0; 
+if(operator == '+'){
+ result = numberr1 + numberr2;
+}else if(operator == '-'){
+ result = numberr1 - numberr2;   
+}else if(operator == '*'){
+    result = numberr1 * numberr2;
+}else if(operator == '/'){
+    result = numberr1 / numberr2;
+}
+console.log(`${numberr1} ${operator} ${numberr2} = ${result}`);
+
+//Find the greatest common divisor of two numbers?
