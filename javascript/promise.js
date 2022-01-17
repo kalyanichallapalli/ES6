8cc4651acd0378235fc72c80600961af8b32cba5
Kalyani <kalyanireddy9@gmail.com>
2022-01-16 23:21:47 -0800

    new

---
+++ javascript/promise.js
@@ -0,0 +1,6 @@
+//The Promise object represents either completion (or failure)
+// of an asynchronous operation and its resulting value or error
+//The Promise object supports two properties: state and result.
+//STATE : PENDING , FULLFILL OR REJECTED 
+//RESULT: UNDEFINED, A RESULT VALUE ,AN ERROR OBJECT 
+function mainProgram()
