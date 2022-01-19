12220b38fcaf565a126c8ad2e648659e1a9aad02
Kalyani <kalyanireddy9@gmail.com>
2022-01-18 17:10:41 -0800

    deleted

--- my-react/Components/Function.js
+++
@@ -1,8 +0,0 @@
-function FunctionComp() {
-    let name = "kalyani";
-    return <dev>
-        <h2>This is the Functional Component Created by{name}</h2>
-    </dev>
-    
-}
-export default FunctionComp;
--- my-react/src/Components/Functionalcomp.js
+++
@@ -1,7 +0,0 @@
-function Welcome() {
-    const name = "kalyani";
-    return <dev>
-        <h1>Functional Components Created By {name}</h1>;
-        </dev>
-  }
-  export default Welcome;
