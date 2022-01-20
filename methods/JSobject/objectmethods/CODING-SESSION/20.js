7693e9ee1c4a7c8aa8bdeb7d07346588fafcd985
Kalyani <kalyanireddy9@gmail.com>
2022-01-20 15:24:28 -0800

    11

--- methods/JSobject/objectmethods/CODING-SESSION/20.js
+++ methods/JSobject/objectmethods/CODING-SESSION/20.js
@@ -14,3 +14,22 @@ if(operator == '+'){
 }
 console.log(`${numberr1} ${operator} ${numberr2} = ${result}`);
 //12)Find the greatest common divisor of two numbers?
+function gcd_two_numbers(x, y) {
+    // if ((typeof x !== 'number') || (typeof y !== 'number')) 
+    //   return false;
+    // x = Math.abs(x);
+    // y = Math.abs(y);
+    while(y) {
+      var t = y;
+      y = x % y;
+      x = t;
+    }
+    return x;
+  }
+  
+  console.log(gcd_two_numbers(8, 12));
+  console.log(gcd_two_numbers(9, 3));
+  console.log(gcd_two_numbers(15, 5));
+  console.log(gcd_two_numbers(2, 4));
+ 
+ 
