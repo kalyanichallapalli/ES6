f90df3b7a43c0666bcb4d9d4d63c4ba4a2d46a8e
Kalyani <kalyanireddy9@gmail.com>
2022-01-19 21:35:01 -0800

    new

--- methods/JSobject/objectmethods/CODING-SESSION/20.js
+++ methods/JSobject/objectmethods/CODING-SESSION/20.js
@@ -1,4 +1,4 @@
-//Write a program that can perform arithmetic operations (calculator)?
+//11)Write a program that can perform arithmetic operations (calculator)?
 let numberr1 = 5
 let numberr2 = 4
 let operator = '+';
@@ -13,4 +13,4 @@ if(operator == '+'){
     result = numberr1 / numberr2;
 }
 console.log(`${numberr1} ${operator} ${numberr2} = ${result}`);
-//Find the greatest common divisor of two numbers?
+//12)Find the greatest common divisor of two numbers?
