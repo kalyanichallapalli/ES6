7dca75dfaae30e3ad591d7a8752b5da898c173d7
Kalyani <kalyanireddy9@gmail.com>
2022-01-22 15:12:52 -0800

    new

--- myapp/src/Conponents/ClassComponent.js
+++ myapp/src/Conponents/ClassComponent.js
@@ -3,11 +3,11 @@ class ClassComponent extends React.Component{
     constructor(){
         super();
         this.name ="Kalyani"
+        
     }
+    
     render() {
-        return<dev>
-     <h2>This is a Class Component Created by {this.name}</h2>
-    </dev>
+        return<dev><h2>This is a Class Component Created by {this.name}</h2></dev>
     }
 
 }
