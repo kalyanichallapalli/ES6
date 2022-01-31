bcb1326d878f1102f7337657f6950bce3f499428
Kalyani <kalyanireddy9@gmail.com>
2022-01-30 21:54:23 -0800

    Refs chage

---
+++ myreactapp/src/UseContext.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class UseContext extends Component {
+    
+  render() {
+    return <div></div>;
+  }
+}
+export default UseContext;
--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,17 +1,15 @@
 
 import './App.css';
 // import ParentComp from './Pure/ParentComp';
-import Post from './AxiosGet/Post';
-
+// import Post from './AxiosGet/Post';
+import RefsDemo from './Pure/RefsDemo';
 function App() {
   return (
     <div className="App">
-     
       {/* <ParentComp></ParentComp> */}
-      <Post></Post>
-      
+      {/* <Post></Post> */}
+      <RefsDemo></RefsDemo>
     </div>
   )
 }
-
 export default App;
