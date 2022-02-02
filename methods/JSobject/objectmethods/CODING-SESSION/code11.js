6baed1cf70e12f1696d552fe6b4cc787a535e4b4
Kalyani <kalyanireddy9@gmail.com>
2022-02-01 21:23:43 -0800

    new

--- methods/JSobject/objectmethods/CODING-SESSION/code11.js
+++ methods/JSobject/objectmethods/CODING-SESSION/code11.js
@@ -16,9 +16,11 @@ function maxoccer(str){
 console.log(maxoccer('KALYANI'))
 
 function stringMap(str){
+    let newstr = str.split(' ')
+    console.log(newstr)
     let mymap = new Map();
-      for (let i =0;i<str.length; i++){
-          mymap.set(str[i], mymap.has(str[i]) ? mymap.get(str[i]) + 1 : 1);
+      for (let i =0;i<newstr.length; i++){
+          mymap.set(newstr[i], mymap.has(newstr[i]) ? mymap.get(newstr[i]) + 1 : 1);
       }
    return mymap;
   }
