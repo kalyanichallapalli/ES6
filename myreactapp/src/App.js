fa8f14874f7d660c01027bc0d173205c531139f2
Kalyani <kalyanireddy9@gmail.com>
2022-02-02 11:08:56 -0800

    del

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,18 +1,11 @@
-import './App.css';
-import React, { createContext } from 'react';
-import ComponentC from './ReactHooks/ComponentC';
-export const UserContext = React.createContext();
-export const CityContext = React.createContext();
 
+import './App.css';
+import React from 'react';
 function App() {
-  console.log("App")
+ 
   return (
     <div className="App">
-   <UserContext.Provider value = {'Kalyani Challapalli'}> 
-   <CityContext.Provider value = {'Mountain House'}>
-   <ComponentC/>
-   </CityContext.Provider>
-   </UserContext.Provider>
+   
      </div>
   );
 }
