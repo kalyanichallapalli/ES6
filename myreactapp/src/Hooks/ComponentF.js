a08790f14452ee5c0a4d8a9c610b70f42d9dd7e8
Kalyani <kalyanireddy9@gmail.com>
2022-02-02 14:10:29 -0800

    new

--- myreactapp/src/Hooks/ComponentF.js
+++ myreactapp/src/Hooks/ComponentF.js
@@ -1,22 +1,15 @@
 import React from 'react';
+import { useContext } from 'react/cjs/react.development';
 import {UserContext} from '../App'
 
 
 function ComponentF() {
+ const username = useContext(UserContext)
   return (
       <div>
-          <UserContext.Consumer>
-              {
-                  username =>{
-                      return <div> 
-                          Component F Consumers {username} from App Component
-                           </div>
-                  }
-              }
-          </UserContext.Consumer>
-      </div>
+         Component F Consumers {username} from App Component 
+   </div>
   )
   
-
 }
   export default ComponentF;
