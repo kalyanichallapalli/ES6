5ac95892c55789130b0798bc766684f68b1ceee2
kalyani <kalyanireddy9@gmail.com>
2022-02-23 10:17:29 -0800

    new

--- methods/JSobject/objectmethods/CODING-SESSION/kumarSession/deepandshallow.js
+++ methods/JSobject/objectmethods/CODING-SESSION/kumarSession/deepandshallow.js
@@ -35,30 +35,35 @@ let obj1 = {
 }
 let obj2 = {
     num1: 30,
-    num2: 70
+    num2: 100000
 }
  obj1 = obj2;
- console.log( 'shallow comparision is ', Object.is(obj1,obj2))
-if(obj1 = obj2){
-console.log ('true');
-} else {
-    console.log( 'false');
-}
-console.log('Are equal? -->', JSON.stringify(obj1) === JSON.stringify(obj2));
+ console.log( 'shallow comparision is...................... ', Object.is(obj1,obj2))
+ console.log('Are equal?....... -->', JSON.stringify(obj1) === JSON.stringify(obj2));
+ if(obj1 === obj2){
+    console.log ('true');
+    } else {
+        console.log( 'false');
+    }
+    
 
 
 let obj3 = {
-    num1: 30,
+    num1: 30, 
     num2: 70
 }
 let obj4 = {
     num2: 30,
     num1: 70
 }
- obj1 = obj2;
  console.log( 'shallow comparision is ', Object.is(obj3,obj4))
  console.log('Are equal? -->', JSON.stringify(obj3) === JSON.stringify(obj4));
-
+ if(obj3 === obj4){
+    console.log ('true');
+    } else {
+        console.log( 'false');
+    }
+    
 
 
 
