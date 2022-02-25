ed065f29f5fa4000ad0a05ff8b86f18faef3f132
kalyani <kalyanireddy9@gmail.com>
2022-02-24 22:50:40 -0800

    new

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
-import Functioncomp from './Conponents/Functioncomponent';
-import ClassComponent from './Conponents/ClassComponent';
-import F1 from './Conponents/props/f1';
-// import Class2 from './Conponents/changestate';
 
 function App() {
   return (
     <div className="App">
       <h2>Welcome to the React World </h2>
-      <Functioncomp></Functioncomp>
-      <ClassComponent></ClassComponent>
-      <F1></F1>
-      {/* <Class2></Class2> */}
+      
     </div>
   );
 }
