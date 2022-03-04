3c333ae78be6454a8872ba008e37cd607b7e3259
kalyani <kalyanireddy9@gmail.com>
2022-03-04 12:21:00 -0800

    w

--- nodejsproject/nodeMailer.js
+++ nodejsproject/nodeMailer.js
@@ -1,1 +1,23 @@
-const 
+const nodemailer = require('nodemailer')
+const transportor = nodemailer.createTransport({
+    service: 'gmail',
+    auth:{
+        user:'kalyanireddy9@gmail.com',
+        password:SNidhi143$
+    }
+});
+
+const mailOptions = {
+    from:'kalyanireddy9@gmail.com',
+    to: 'kchallapalli@gmail.com',
+    subject: 'sending for kalyanireddy9 to challapalli',
+    text: 'hallow nodejs'
+}
+
+transportor.sendMail(mailOptions,(err,data) =>{
+    if(err){
+        console.log('error')
+    }else{
+        console.log('email sent')
+    }
+})
